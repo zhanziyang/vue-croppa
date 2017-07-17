77a8a748a9966353b6808746fdf2290f06817096
zhanziyang <zhanziyang126@hotmail.com>
2017-07-17 20:47:15 +0800

    expose component

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-import cropper from './cropper.vue'
+import component from './cropper.vue'
+import assign from 'object-assign'
+
+const defaultOptions = {
+  componentName: 'croppa'
+}
 
 const VueCroppa = {
   install: function (Vue, options) {
-    options = options || {}
+    options = assign({}, defaultOptions, options)
     let version = Number(Vue.version.split('.')[0])
     if (version < 2) {
       throw new Error(`vue-croppa supports vue version 2.0 and above. You are using Vue@${version}. Please upgrade to the latest version of Vue.`)
     }
     let componentName = options.componentName || 'croppa'
-    Vue.component(componentName, cropper)
+
+    // registration
+    Vue.component(componentName, component)
   }
 }
 
+export { component }
 export default VueCroppa
