26fa1fa7a2494067f1ee88884f1e3b3d2d63f77a
zhanziyang <zhanziyang126@hotmail.com>
2017-08-09 11:45:23 +0800

    doc webpack publicPath fix

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/vue-croppa/dist/',
     filename: 'build.js'
   },
   externals: {
