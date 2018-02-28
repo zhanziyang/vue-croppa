961cd82956dbb2395037f7fda135426776efcd9a
Gabriel Sosa <gabriel@stensul.com>
2018-02-28 08:40:15 -0300

    return false instead in order to abort the flow

--- src/cropper.vue
+++ src/cropper.vue
@@ -642,11 +642,13 @@
         if (!this._fileSizeIsValid(file)) {
           this.loading = false
           this.$emit(events.FILE_SIZE_EXCEED_EVENT, file)
+          return false
         }
         if (!this._fileTypeIsValid(file)) {
           this.loading = false
           this.$emit(events.FILE_TYPE_MISMATCH_EVENT, file)
           let type = file.type || file.name.toLowerCase().split('.').pop()
+          return false
         }
         if (typeof window !== 'undefined' && typeof window.FileReader !== 'undefined') {
           let fr = new FileReader()
