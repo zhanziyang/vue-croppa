ccbf39239328e506f9431cd3378b95b5e8f67f9a
zhanziyang <zhanziyang126@hotmail.com>
2026-09-23 14:06:45 +0800

    docs(v2): register foundation lab component

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import DemoFrame from '../../components/DemoFrame.vue'
+import V2FoundationLab from '../../components/V2FoundationLab.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('DemoFrame', DemoFrame)
+    app.component('V2FoundationLab', V2FoundationLab)
   },
 }
